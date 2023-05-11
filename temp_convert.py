cdad67d9296b646ca357734a848f0f2e6e7f549c
Ben Barkhouse <bfbarkhouse@live.com>
2023-05-11 15:20:29 -0400

    added logging

--- temp_convert.py
+++ temp_convert.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename='temp_convert.log', encoding='utf-8', level=logging.DEBUG)
 
 @app.route('/')
 def home():
@@ -9,22 +11,25 @@ def home():
 @app.route('/', methods=['GET', 'POST'])
 def results():
     user_input = request.form['user_input']
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     if not user_input:
+        logging.info('Request from %s did not enter a number', ip_addr)
         return render_template('index.html', result_container='notification is-primary', error='Please enter a number.')
     else:
         if request.form["unit"] == "celsius":
-            return convert_to_celsius(user_input)
+            return convert_to_celsius(user_input, ip_addr)
         elif request.form["unit"] == "fahrenheit":
-            return convert_to_fahrenheit(user_input)
+            return convert_to_fahrenheit(user_input, ip_addr)
 
-
-def convert_to_celsius(user_input): 
+def convert_to_celsius(user_input, ip_addr): 
+    logging.info('User at %s entered %s', ip_addr, user_input)
     celsius = float(user_input)
     celsius = (celsius - 32) / 1.8
     celsius = '{:.1f}'.format(celsius)
     return render_template('index.html', title='Results', input=user_input, from_unit='Fahrenheit is', to_unit='in Celsius', result_container='notification is-primary', result=celsius)
 
-def convert_to_fahrenheit(user_input):  
+def convert_to_fahrenheit(user_input, ip_addr):
+    logging.info('User at %s entered %s', ip_addr, user_input)  
     fahrenheit = float(user_input)
     fahrenheit = (fahrenheit * 9/5) + 32
     fahrenheit = '{:.1f}'.format(fahrenheit)
