69078d6bc0f085c9b1c4ba6d10320c10802d6dab
Ben Barkhouse <bfbarkhouse@live.com>
2023-05-11 22:41:29 -0400

    testing

--- terraform/az_webapp.tf
+++ terraform/az_webapp.tf
@@ -20,17 +20,17 @@ variable "pat" {
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "bfb-webapp-rg1"
-  location = "eastus"
+  name     = "bfb-temperature-py-rg"
+  location = "eastus2"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-temperature-py"
+  name                = "appserviceplan-temperature-py"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 
 # Create the web app, pass in the App Service Plan ID
@@ -42,8 +42,11 @@ resource "azurerm_linux_web_app" "webapp" {
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
-    use_32_bit_worker = true
-    always_on = false
+    #use_32_bit_worker = true
+    #always_on = false
+    application_stack {
+    python_version = "3.9"
+  }  
   }
   logs {
     application_logs {
