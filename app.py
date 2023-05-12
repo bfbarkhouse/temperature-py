0bff4eec4ff478373a6f47c667166d342ad26499
Ben Barkhouse <bfbarkhouse@live.com>
2023-05-11 23:45:03 -0400

    added error handling when a unit is not selected

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def results():
         logging.info('Request from %s did not enter a number', ip_addr)
         print(f'Request from {ip_addr} did not enter a number')
         return render_template('index.html', result_container='notification is-primary', error='Please enter a number.')
+    elif 'unit' not in request.form:
+        logging.info('Request from %s did not select a unit', ip_addr)
+        print(f'Request from {ip_addr} did not select a unit')
+        return render_template('index.html', result_container='notification is-primary', error='Please select a unit.')
     else:
         if request.form["unit"] == "celsius":
             return convert_to_celsius(user_input, ip_addr)
@@ -39,4 +43,4 @@ def convert_to_fahrenheit(user_input, ip_addr):
     return render_template('index.html', title='Results', input=user_input, from_unit='Celsius is', to_unit=' in Fahrenheit', result_container='notification is-primary', result=fahrenheit)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
